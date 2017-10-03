eadab3c016ca0553d98866179edd3152303010b3
jb1361 <justinbutler4@hotmail.com>
2017-10-03 15:26:40 -0400

    reasearch work
    having problems with appending a dataframe for whatever reason

--- predictionTesting/predict.py
+++ predictionTesting/predict.py
@@ -2,9 +2,12 @@ import time
 import sqlite3
 import pandas as pd
 import numpy as np
-
+import scipy as sp
+from scipy import stats
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+'''
 from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import (RandomTreesEmbedding, RandomForestClassifier, GradientBoostingClassifier)
@@ -12,7 +15,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_curve
 from sklearn.pipeline import make_pipeline
-
+'''
 traindataframes = []
 testDataFrame = []
 
@@ -22,22 +25,80 @@ def predict():
 
 
 
-def start_predict():
+def cost():
+	cost = 0
+	return cost
+	
+	
+def gradient_descent():
 	global traindataframes
 	global testDataFrame
-	initial_df = pd.DataFrame(traindataframes[0])
 	
+	#get initial data
+	initial_df = pd.DataFrame(traindataframes[0])
 	id_col = initial_df['Id']
 	price_col = initial_df['Current_price']
 	trend_col = initial_df['Current_trend']
 	today_price_col = initial_df['Today_price']
 	today_trend_col = initial_df['Today_trend']
-	print(id_col)
-	print(price_col) 
-	print(price_trend_col)
-	print(today_price_col) 
-	print(today_price_col)	
+	#price_col = pd.concat()
+	today_price_col = today_price_col.columns
+	
+	
+	#convert data into usable form and put into dataframe
+	pd.to_numeric(price_col,downcast = 'float')
+	data_df = [price_col,today_price_col]
+	data_df = pd.DataFrame(data_df,dtype = 'float')
+	data_df = pd.DataFrame.transpose(data_df)
+	
+	#get features
+	features = data_df
+	features = (features - features.mean())/features.std()
+	
+	#values
+	value_price = [traindataframes[1]['Current_price']]
+	value_today = [traindataframes[1]['Today_price']]
+	values = [value_price,value_today]
+	values = pd.DataFrame(values,dtype = 'float')
+	print(values)
+	values =(values - values.mean())/values.std()
+	m = len(features)
+	#features['ones'] = np.ones(m)
+	
 
+	features_array = np.array(features)
+	values_array = np.array(values)
+
+	alpha = 0.1
+	num_iterations = 20
+	theta_descent = np.zeros(len(features.columns))
+
+	cost_history = []
+
+	for i in range(num_iterations):
+		predicted_value = np.dot(features_array, theta_descent)
+		theta_descent = theta_descent + alpha/m * np.dot(values_array - predicted_value, features_array)
+		sum_of_square_errors = np.square(np.dot(features_array, theta_descent) - values_array).sum()
+		cost = sum_of_square_errors / (2 * m)
+		cost_history.append(cost)
+
+	cost_history = pd.Series(cost_history)
+	predictions = np.dot(features_array, theta_descent)
+
+	print('============================================')
+	print('Cost History: ', cost_history)
+	print('Predictions: ',predictions)
+	print('Alpha: ', alpha)
+	print('Iterations: ',num_iterations)
+	#===========================
+	#7 ===================================
+	data_predictions = np.sum((values - predictions)**2)
+	mean = np.mean(values)
+	sq_mean = np.sum((values - mean)**2)
+
+	r = 1 - data_predictions / sq_mean
+	print('R: ', r)
+	print()
 
 
 def get_Data():
@@ -72,5 +133,5 @@ def get_Data():
 
 	
 get_Data()
-start_predict()
+gradient_descent()
 #predict()
