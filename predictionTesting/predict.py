d136042f7eea09569725b7d517edb61d8df74f69
jb1361 <justinbutler4@hotmail.com>
2017-10-24 16:52:39 -0400

    more work on prediction (actualy doing prediction now)

--- predictionTesting/predict.py
+++ predictionTesting/predict.py
@@ -19,97 +19,72 @@ from sklearn.pipeline import make_pipeline
 traindataframes = []
 testDataFrame = []
 
+#def predict_next_day():
+	#return 0
 
-def predict():
-	return
-
-
-
-def cost():
-	cost = 0
-	return cost
-	
-	
 def gradient_descent():
 	global traindataframes
 	global testDataFrame
 	prediction_frame = testDataFrame[0]
 	prediction_frame = prediction_frame['Current_price']
+	prediction_frame = prediction_frame[0:50]
+	#prediction_frame = (prediction_frame - prediction_frame.mean())/prediction_frame.std()
+	#prediction_frame = np.array(prediction_frame)
 	
-	initial_df = traindataframes[0]
-	
-	price_col = initial_df['Current_price']
-	
-	
-
-
-	
-	'''
-	#remove +signs
-	for i in value_today:
-		if '+' in i:
-			temp = i.replace('+','')
-			value_today = value_today.replace(str(i),temp)
-	#convert to floats
-	for i in value_today:
-		value_today = value_today.replace(str(i),float(i))
-	'''
 	frames = []
 	for i in traindataframes:
 		temp = i['Current_price']
 		frames.append(temp[0:50])
-		
-	'''	#remove +signs
-	for i in frames:
-		for j in i:
-			if '+' in j:
-				temp = j.replace('+','')
-				j = j.replace(str(i),temp)	
-	#convert to floats
-	for i in frames:
-		for j in i:
-			j = j.replace(str(j),float(j))
-			'''
+		'''
+	frames = []
+	for i in traindataframes:
+		temp = i['Today_price']
+		for i in temp:
+			if '+' in i:
+				t = i.replace('+','')
+				temp = temp.replace(str(i),t)
+		for i in temp:
+			temp = temp.replace(str(i),float(i))
+		frames.append(temp[0:50])
+		'''
 	#print(frames)
 	data_df = pd.concat(frames,axis=1)
 	data_df.columns = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']
-	print(data_df)
+	
 	
 	#get features
 	features = data_df
 	features = (features - features.mean())/features.std()
 	features_array = np.array(features)
 	
+	
 	#values_array = np.random.random_sample(50)
-	values_array = prediction_frame[0:50]
-	values_array = (values_array - values_array.mean())/values_array.std()
-	values_array = np.array(values_array)
+	values = prediction_frame
+	values = (values - values.mean())/values.std()
+	values_array = np.array(values)
+	
 	m = len(values_array)
 	alpha = 0.01
 	num_iterations = 100
 	
 	theta_descent = np.zeros(len(features.columns))
 	
-	'''theta_descent = [ 0.04983402,  0.04984066,  0.04997515,  0.0499784,   0.0002391,   0.04998249,
-	0.04996475,  0.05001895,  0.05001934 , 0.04997175,  0.04997307,  0.04997442,
-	0.04998315, 0.05007499,  0.05008444,  0.05008444,  0.05002506,  0.05002304,
-	0.05002437,  0.05002436,  0.05018056]
-	'''
 	cost_history = []
 
 	for i in range(num_iterations):
-		#print('Iteration: ' + str(i))
 		predicted_value = np.dot(features_array, theta_descent)
 		theta_descent = theta_descent + alpha/m * np.dot(values_array - predicted_value, features_array)
 		sum_of_square_errors = np.square(np.dot(features_array, theta_descent) - values_array).sum()
 		cost = sum_of_square_errors / (2 * m)
 		cost_history.append(cost)
 
+		
+	#all output and debugging 
 	cost_history = pd.Series(cost_history)
-	predictions = np.dot(features_array, theta_descent).transpose()
+	
+	predictions = np.dot(features, theta_descent).transpose()
 	print('============================================')
 	print('Cost History: ', cost_history)
-	print('Predictions: ',predictions)
 	print('Theta Descent: ',theta_descent)
 	print('Alpha: ', alpha)
 	print('Iterations: ',num_iterations)
@@ -119,14 +94,20 @@ def gradient_descent():
 	sq_mean = np.sum((values_array - mean)**2)
 	r = 1 - data_predictions / sq_mean
 	print('R: ', r)
-	print()
-	print('============================================')
 	
-	df = frames[-1:]
-	plot_prediction = np.add(df[0],predictions)
+
+	
+	#denormalize data
+	features = ((features * data_df.std()) + data_df.mean())
+	print(features)
+	predictions = np.dot(features, theta_descent).transpose()
+	print('Predictions: ',predictions)
+	print('============================================')
+
 	fig, ax = plt.subplots()
-	ax.plot(prediction_frame[0:50],'o',markersize = 1, color = 'green')
-	ax.plot(plot_prediction,'o',markersize = 1, color = 'blue')
+	ax.plot(prediction_frame,'o',markersize = 1, color = 'green', label = 'Actual Price')
+	ax.plot(predictions,'o',markersize = 1, color = 'blue', label = 'Predicted Price')
+	#ax.plot(features,'o',markersize = 1, color = 'red', label = 'Price Previously')
 	fig2, ax2 = plt.subplots()
 	ax2.plot(cost_history,'o',markersize = 1, color = 'blue')
 	plt.show()
